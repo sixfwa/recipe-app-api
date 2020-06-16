aadeb71c1a835cd95ebbe0614d73fa3062423169
sixfwa <farhanaliw6@gmail.com>
2020-06-16 22:52:35 +0100

    Configured postgres db

---
+++ app/core/tests/test_command.py
@@ -0,0 +1,24 @@
+from unittest.mock import patch
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import TestCase
+
+
+class CommandTests(TestCase):
+
+    def test_wait_for_db_ready(self):
+        """Test waiting for database when database is available"""
+        # Mocking the __getitem__ function
+        with patch("django.db.utils.ConnectionHandler.__getitem__") as gi:
+            gi.return_value = True
+            call_command("wait_for_db")
+
+            self.assertEqual(gi.call_count, 1)
+
+    def test_wait_for_db(self):
+        """Test waiting for database"""
+        with patch("django.db.utils.ConnectionHandler.__getitem__") as gi:
+            gi.side_effect = [OperationalError] * 5 + [True]
+            call_command("wait_for_db")
+            self.assertEqual(gi.call_count, 6)
